a125f7756d2542dcf2e206da54c623e3392f0363
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-14 00:03:24 +0100

    delete done

--- backend.js
+++ backend.js
@@ -1,12 +1,37 @@
 import express from 'express';
+import mongodb, { MongoClient } from 'mongodb';
 
 const app = express();
 const port = 3016;
+const mongoConnectString = 'mongodb://localhost:27017';
+const client = new MongoClient(mongoConnectString);
+
+const execMongo= async (done) => {
+	await client.connect();
+	const db = client.db('api001');
+	done(db);
+}
 
 app.get('/', (req, res) => {
-	res.json({
-		message: "this works"
-	})
+	execMongo(async (db) => {
+		const users = await db.collection('users100').find()
+			.project({
+				name: 1,
+				username: 1,
+				email: 1
+			}).toArray();
+		res.json(users);
+	});
+});
+
+app.delete('/delete/:id', (req, res) => {
+	const id = req.params.id;
+	execMongo(async (db) => {
+		const deleteResult = await db.collection('users100').deleteOne({ _id: new mongodb.ObjectId(id) });
+		res.json({
+			result: deleteResult
+		});
+	});
 });
 
 app.listen(port, () => {
